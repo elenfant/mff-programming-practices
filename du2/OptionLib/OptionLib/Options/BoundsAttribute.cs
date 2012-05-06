6c395a5f6b265dd161cb52d0aecf80d0ec1ffcf7
Michal Krkavec <michal.krkavec@seznam.cz>
2012-05-06 23:52:21 +0200

    tak ted ty meze...

---
+++ du2/OptionLib/OptionLib/Options/BoundsAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//TODO: predelat na int i float, tj. zadávat hodnotu attributu jako string
+namespace OptionLib
+{
+    /// <summary>
+    /// Bounds used for corresponding integer option.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public sealed class BoundsAttribute : Attribute
+    {
+        public BoundsAttribute()
+            : base() {
+                LowerBound = int.MinValue;
+                UpperBound = int.MaxValue;
+        }
+
+        public int LowerBound
+        {
+            get;
+            set;
+        }
+
+        public int UpperBound
+        {
+            get;
+            set;
+        }
+    }
+}
